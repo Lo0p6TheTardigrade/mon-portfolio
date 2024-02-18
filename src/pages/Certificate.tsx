f1b602d20d7f547329084359e5a8759282ba3603
Lo0p6TheTardigrade <rezonance30@gmail.com>
2024-02-18 22:55:28 +0100

    Add Layout to Certificate page

--- src/pages/Certificate.tsx
+++ src/pages/Certificate.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Proof from '../components/Proof';
 import { Link } from 'react-router-dom';
 import Box from '../components/modules/Components';
+import Layout from '../components/Layout';
 // import { useSelector } from 'react-redux';
 // import { Behavior_Custom_Interface } from '../types';
 
@@ -14,25 +15,27 @@ const Certificate = (props: Props) => {
   const carret = require('../assets/images/carretLeft.webp');
 
   return (
-    <Box
-      tag="div"
-      class="certificate-page-container">
-      <Proof
-        cegefos={{ file: cegefos, name: 'Cegefos' }}
-        openclassroom={{ file: openclassroom, name: 'OpenClassroom' }}
-        image={{ src: carret, alt: 'bouton switcher', class: 'btn-file-switcher' }}
-        // image={{ src: carret, alt: 'bouton switcher', class: `btn-file-switcher ${window5 && 'rotate-animate-reverse'}` }}
-      />
+    <Layout>
       <Box
         tag="div"
-        class="btn-back-to-home-container">
-        <Link
-          to="/"
-          className="btn-back-to-home">
-          Retour
-        </Link>
+        class="certificate-page-container">
+        <Proof
+          cegefos={{ file: cegefos, name: 'Cegefos' }}
+          openclassroom={{ file: openclassroom, name: 'OpenClassroom' }}
+          image={{ src: carret, alt: 'bouton switcher', class: 'btn-file-switcher' }}
+          // image={{ src: carret, alt: 'bouton switcher', class: `btn-file-switcher ${window5 && 'rotate-animate-reverse'}` }}
+        />
+        <Box
+          tag="div"
+          class="btn-back-to-home-container">
+          <Link
+            to="/"
+            className="btn-back-to-home">
+            Retour
+          </Link>
+        </Box>
       </Box>
-    </Box>
+    </Layout>
   );
 };
 
